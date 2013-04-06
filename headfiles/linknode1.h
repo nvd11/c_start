360ed269fad70c93dd6da2ed05a876fa11ce124c
gateman <nvd11@163.com>
2013-04-06 17:08:07 +0800

    link1

---
+++ headfiles/linknode1.h
@@ -0,0 +1,28 @@
+#include "bool_me.h"
+#ifndef __LINKTABLE1_H_H
+#ifndef __LINKTABLE1_H_H
+
+	struct person{
+		int id;
+		char * name[];
+		struct person * pnext;
+	};
+	
+	typedef struct person PERSON;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
